37d36b6843e98b776d07b4245d93126bf8147e8c
molly-ane <cassesquivel23@gmail.com>
2023-04-20 19:55:03 +0800

    Added update and Create

--- File Handling by Group/Form1.vb
+++ File Handling by Group/Form1.vb
@@ -2,37 +2,60 @@
 
 Public Class Form1
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnupdate.Click
-        Dim totss As Integer = CInt(txtprices.Text) * CInt(txtqtyss.Text)
-
-        If Not File.Exists(filepath) Then
-            CreateFile(filepath)
+        If Not SelectItem() Then
+            Return
         End If
 
-        Dim data As String = txtids.Text + "," + txtnamess.Text + "," + txtqtyss.Text + "," + txtprices.Text + "," + totss.ToString("C2")
-        data = txtids.Text + "=" + data
-        UpdateFile(filepath, data)
+        If IsEmptyInputs({txtids, txtnamess, txtprices, txtqtyss}) Then
+            Return
+        End If
+        If Not IsValidNumbers({txtprices, txtqtyss}) Then
+            Return
+        End If
 
-        txttots.Text = totss.ToString("C2")
+        txttots.Text = (CDec(txtprices.Text) * CDec(txtqtyss.Text)).ToString("f2")
 
+        Dim datas As String() = GetInputs({txtids, txtnamess, txtprices, txtqtyss, txttots})
+        If datas Is Nothing Then
+            Return
+        End If
+
+        Dim item As String = txtids.Text + "=" + ConcatStrings(datas)
+
+        Updateitems(item, currentItemId)
+        dgrid1.Rows.RemoveAt(itemPos)
+        dgrid1.Rows.Insert(itemPos)
         GridFile(dgrid1)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btndel.Click
         Dim id As String = InputBox("Enter The Product Id", "Product Id")
-        Deletefile(filepath, id)
+        Deleteitems(filepath, id)
         GridFile(dgrid1)
     End Sub
-
-
 
     Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles btnread.Click
         GridFile(dgrid1)
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btncreate.Click
-        If Not File.Exists(filepath) Then
-            CreateFile(filepath)
+        If IsEmptyInputs({txtids, txtnamess, txtprices, txtqtyss}) Then
+            Return
         End If
+        If Not IsValidNumbers({txtprices, txtqtyss}) Then
+            Return
+        End If
+
+        txttots.Text = (CDec(txtprices.Text) * CDec(txtqtyss.Text)).ToString("f2")
+
+        Dim datas As String() = GetInputs({txtids, txtnamess, txtprices, txtqtyss, txttots})
+        If datas Is Nothing Then
+            Return
+        End If
+
+        Dim item As String = txtids.Text + "=" + ConcatStrings(datas)
+        CreateItem(item + vbCrLf)
+        GridFile(dgrid1)
     End Sub
 
     Private Sub btnsearch_Click(sender As Object, e As EventArgs) Handles btnsearch.Click
@@ -48,4 +71,26 @@ Public Class Form1
         txttots.Clear()
 
     End Sub
+
+    Public Function SelectItem() As Boolean
+        Dim id As String
+        If dgrid1.SelectedRows.Count > 0 Then
+            id = dgrid1.SelectedRows(0).Cells(0).Value
+        Else
+            id = InputBox("Enter Item ID", "Get Item")
+            If String.IsNullOrEmpty(id) Then
+                Return 0
+            End If
+
+        End If
+
+        itemPos = IndexFromGrid(dgrid1, id)
+        If itemPos < 0 Then
+            MsgBox("Item Not Found", MsgBoxStyle.Critical, "Error")
+            Return 0
+        End If
+        currentItemId = id
+        SelectedItem = ReadItem(currentItemId)
+        Return 1
+    End Function
 End Class
--- File Handling by Group/Module1.vb
+++ File Handling by Group/Module1.vb
@@ -1,10 +1,10 @@
 ﻿Imports System.ComponentModel
 Imports System.IO
 Module Module1
-    Dim ids As Integer
-    Dim names As String
-    Dim qty As Integer
-    Dim pricy As Integer
+
+    Public itemPos As Integer
+    Public currentItemId As String
+    Public SelectedItem As String
 
     Public filepath As String = CurDir() + "\PRODUCT.txt"
 
@@ -12,25 +12,39 @@ Module Module1
         File.Create(filepath).Dispose()
     End Sub
 
-    Public Sub UpdateFile(filepath As String, text As String)
-        If Not String.IsNullOrEmpty(text) Then
-            Dim data As String() = Readfile(filepath)
-            For i As Integer = 0 To data.Length - 1
-                Dim line As String = data(i)
-                If line.Contains(text.Split("=")(0) + "=") Then
-                    MsgBox("Product Already Existed!")
-                    Return
-                End If
-            Next
-            File.AppendAllText(filepath, text + vbCrLf)
-
+    Public Sub CreateItem(item As String)
+        If Not String.IsNullOrEmpty(item) Then
+            File.AppendAllText(filepath, item)
         End If
     End Sub
-    Public Sub Deletefile(filepath As String, id As String)
+    Public Sub Updateitems(text As String, id As Integer)
+
+
+        If String.IsNullOrEmpty(id) Or Not IsNumeric(id) Then
+            Return
+        End If
+
+        Dim datas As ArrayList = New ArrayList(Readitems())
+        Dim i = 0
+
+        While i < datas.Count
+            Dim lineId As String = datas(i).Split("=")(0)
+            If lineId = id Then
+
+                datas(i) = text
+                Dim new_datas As String() = datas.ToArray(GetType(String))
+                File.WriteAllLines(filepath, new_datas)
+
+                Return
+            End If
+            i += 1
+        End While
+    End Sub
+    Public Sub Deleteitems(filepath As String, id As String)
         If String.IsNullOrEmpty(id) Then
             Return
         End If
-        Dim data As String() = Readfile(filepath)
+        Dim data As String() = Readitems()
         For i As Integer = 0 To data.Length - 1
             Dim line As String = data(i)
             If line.Contains(id + "=") Then
@@ -41,29 +55,57 @@ Module Module1
     End Sub
 
 
-    Public Function Readfile(filepath As String) As String()
+    Public Function Readitems() As String()
         Return File.ReadAllLines(filepath)
     End Function
 
-    Public Sub GridFile(ByRef grid As DataGridView)
-        Dim data As String() = Readfile(filepath)
-            grid.Rows.Clear()
-        For i As Integer = grid.RowCount To data.Length - 1
-            If String.IsNullOrEmpty(data(i).Trim) Then
-                Continue For
-            End If
-            Dim line As String = data(i).Split("=")(1)
+    Public Function ReadItem(id As String) As String
+        If String.IsNullOrEmpty(id) Or Not IsNumeric(id) Then
+            Return ""
+        End If
 
-            Dim array As String() = line.Split(",")
-            grid.Rows.Add(array)
+        Dim datas As String() = Readitems()
+        Dim i As Integer = 0
+        Dim item As String = ""
+
+        While i < datas.Length
+            Dim lineId As String = datas(i).Split("=")(0)
+            If lineId = id Then
+                item = datas(i).Split("=")(1)
+                Return item
+            End If
+            i += 1
+        End While
+        Return item
+    End Function
+
+    Public Sub GridFile(ByRef grid As DataGridView)
+        Dim datas As String() = Readitems()
+        If Not datas.Length - grid.RowCount = 1 Then
+            grid.Rows.Clear()
+        End If
+        For row As Integer = grid.RowCount To datas.Count - 1
+            Dim item As String() = datas(row).Split("=")(1).Split(",")
+            grid.Rows.Add(item)
         Next
     End Sub
+
+    Public Function IndexFromGrid(grid As DataGridView, id As Integer) As Integer
+        Dim i = 0
+        While i < grid.Rows.Count
+            If grid.Rows(i).Cells(0).Value = id Then
+                Return i
+            End If
+            i += 1
+        End While
+        Return -1
+    End Function
 
     Public Sub SearchFile(filepath As String, id As String, ByRef grid As DataGridView)
         If String.IsNullOrEmpty(id) Then
             Return
         End If
-        Dim data As String() = Readfile(filepath)
+        Dim data As String() = Readitems()
 
         For i As Integer = 0 To data.Length - 1
             Dim line As String = data(i)
@@ -74,4 +116,44 @@ Module Module1
         Next
 
     End Sub
+
+    Public Function ConcatStrings(str As String()) As String
+        Dim result As String = ""
+        For Each s In str
+            result += s + ","
+        Next
+        Return result.Remove(result.Length - 1)
+    End Function
+
+    Public Function GetInputs(ByRef inputs As TextBox()) As String()
+        Dim datas As New ArrayList
+
+        For Each t In inputs
+            datas.Add(t.Text)
+        Next
+        Return datas.ToArray(GetType(String))
+    End Function
+
+    Public Function IsEmptyInputs(ByRef inputs As TextBox()) As Boolean
+        For Each t In inputs
+            If String.IsNullOrEmpty(t.Text) Then
+                Dim msg As String = "Found missing value: " & t.Name.Substring(3)
+                MsgBox(msg, MsgBoxStyle.Exclamation, "Please fill all the fields")
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Public Function IsValidNumbers(ByRef inputs As TextBox()) As Boolean
+        For Each t In inputs
+            If Not IsNumeric(t.Text) Then
+                Dim msg As String = "Found invalid value: " & t.Name.Substring(3)
+                MsgBox(msg, MsgBoxStyle.Exclamation, "Invalid Value")
+                t.Focus()
+                Return False
+            End If
+        Next
+        Return True
+    End Function
 End Module
